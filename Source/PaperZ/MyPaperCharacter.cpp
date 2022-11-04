664f5d7a5683d81603d72df208ce73660592e589
Luben Lubenov <65513534+Moonriderx@users.noreply.github.com>
2022-11-05 01:26:41 +0200

    Replicating

--- Source/PaperZ/MyPaperCharacter.cpp
+++ Source/PaperZ/MyPaperCharacter.cpp
@@ -2,20 +2,40 @@
 
 
 #include "MyPaperCharacter.h"
+#include "PaperFlipbookComponent.h"
+#include "Components/CapsuleComponent.h"
 
 AMyPaperCharacter::AMyPaperCharacter()
 {
+	OnCharacterMovementUpdated.AddDynamic(this, &AMyPaperCharacter::Animate);
 
+	bReplicates = true;
+
+	GetCapsuleComponent()->SetCapsuleRadius(70.f);
+
+	GetSprite()->SetRelativeScale3D(FVector(11.f, 11.f, 11.f));
+	GetSprite()->SetUsingAbsoluteRotation(true);
+	GetSprite()->SetFlipbook(FB.IdleDown);
+	GetSprite()->CastShadow = true;
 }
 
 void AMyPaperCharacter::BeginPlay()
 {
+	Super::BeginPlay();
+
+	SetReplicateMovement(true);
 }
 
 void AMyPaperCharacter::Animate(float DeltaTime, FVector OldLocation, const FVector OldVelocity)
 {
+
+	TOptional<FMinimalViewInfo> ViewInfo;
+
+	/*Everytime we move animate is called and we first call that*/
+	SetCurrentAnimationDirectory(OldVelocity, ViewInfo);
 }
 
-void AMyPaperCharacter::SetCurrentAnimationDirectory(const FVector& Velocity)
+void AMyPaperCharacter::SetCurrentAnimationDirectory(const FVector& Velocity, TOptional<FMinimalViewInfo> ViewInfo)
 {
 }
+
--- Source/PaperZ/MyPaperCharacter.h
+++ Source/PaperZ/MyPaperCharacter.h
@@ -24,8 +24,7 @@ protected:
 	UFUNCTION(BlueprintCallable, Category = "PaperCharacter|Animation")
 		void Animate(float DeltaTime, FVector OldLocation, const FVector OldVelocity);
 
-	UFUNCTION(BlueprintCallable, Category = "PaperCharacter|Animation")
-		virtual void SetCurrentAnimationDirectory(const FVector& Velocity);
+		virtual void SetCurrentAnimationDirectory(const FVector& Velocity, TOptional<FMinimalViewInfo> ViewInfo);
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PaperCharacter|Config")
 		uint8 bIsMoving : 1;
