896155e3264d11decde028daf5e1605ee6a79e0a
Luben Lubenov <Luben1993@abv.bg>
2022-11-23 23:10:42 +0200

    Update MovableStaticMeshComponent.cpp

--- Source/PaperZ/Components/MovableStaticMeshComponent.cpp
+++ Source/PaperZ/Components/MovableStaticMeshComponent.cpp
@@ -5,21 +5,51 @@
 
 UMovableStaticMeshComponent::UMovableStaticMeshComponent()
 {
+	// by default the component is not set to tick. Need to be enabled
+	PrimaryComponentTick.bCanEverTick = true;
+}
 
+
+void UMovableStaticMeshComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (MoveCurve)
+	{
+		FOnTimelineFloat TimelineCallback;
+		FOnTimelineEventStatic TimelineFinishCallback;
+
+		TimelineCallback.BindUFunction(this, FName("OnMove"));  // bind a current function
+		TimelineFinishCallback.BindUFunction(this, FName("OnMoveFinished"));
+
+		MoveTimeline.AddInterpFloat(MoveCurve, TimelineCallback);
+		MoveTimeline.SetTimelineFinishedFunc(TimelineFinishCallback);
+	}
 }
 
 void UMovableStaticMeshComponent::TickComponent(float DeltaTime, ELevelTick Tick, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, Tick, ThisTickFunction);
+
+	if (bIsMoving)
+	{
+		MoveTimeline.TickTimeline(DeltaTime); // move the timeline along
+	}
 }
 
-void UMovableStaticMeshComponent::BeginPlay()
-{
-	Super::BeginPlay();
-}
 
 void UMovableStaticMeshComponent::Move(bool bTriggered)
 {
+	bIsTriggered = bTriggered;
+
+	if (bTriggered)
+	{
+		MoveTimeline.PlayFromStart();
+	}
+	else
+	{
+		MoveTimeline.Reverse();
+	}
 }
 
 void UMovableStaticMeshComponent::OnMove()
