7b8c8ebf238c3ccef676b31e448c9bacecbeb115
Luben Lubenov <Luben1993@abv.bg>
2022-11-24 10:14:15 +0200

    Updating PressurePlateTrigger

--- Source/PaperZ/Props/PressurePlateTrigger.cpp
+++ Source/PaperZ/Props/PressurePlateTrigger.cpp
@@ -2,6 +2,7 @@
 
 
 #include "PressurePlateTrigger.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 APressurePlateTrigger::APressurePlateTrigger()
@@ -9,12 +10,24 @@ APressurePlateTrigger::APressurePlateTrigger()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	SetRootComponent(BaseMesh);
+
+	MovableMesh = CreateDefaultSubobject<UMovableStaticMeshComponent>(TEXT("Movable Mesh"));
+	MovableMesh->SetupAttachment(BaseMesh);
+
+	OverlapComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Overlap Area"));
+	OverlapComponent->SetupAttachment(MovableMesh);
+
 }
 
 // Called when the game starts or when spawned
 void APressurePlateTrigger::BeginPlay()
 {
 	Super::BeginPlay();
+
+	OverlapComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBeginOverlap);
+	OverlapComponent->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnEndOverlap);
 	
 }
 
@@ -23,5 +36,13 @@ void APressurePlateTrigger::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void APressurePlateTrigger::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void APressurePlateTrigger::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
 }
 
--- Source/PaperZ/Props/PressurePlateTrigger.h
+++ Source/PaperZ/Props/PressurePlateTrigger.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/SphereComponent.h"
+#include "PaperZ/Components/MovableStaticMeshComponent.h"
 #include "PressurePlateTrigger.generated.h"
 
 
@@ -23,5 +25,35 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+private:
+	void Interact(bool bIsInteracting);
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Trigger")
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Trigger")
+	UMovableStaticMeshComponent* MovableMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Trigger")
+	USphereComponent* OverlapComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Trigger")
+	bool bIsDisabled; // control the state of the pressurePlate
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Trigger")
+	bool bResetTrigger; // two situations: 1) pressureplate is staying down after activating; 2) pp is going up and resets after OnCollisonExit for example
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Trigger")
+	TArray<FName> TargetTags; // references of tags of actors that we want this pressureplate to affect in the world
+
+
 
 };
