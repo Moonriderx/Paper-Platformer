f0ab3f4d8cc38c3b4fb5cf29e280dff54f74a0e5
Luben Lubenov <Luben1993@abv.bg>
2022-11-24 10:55:15 +0200

    Finish Implementation of Movables

--- Source/PaperZ/Props/MovingStaticMeshBaseActor.cpp
+++ Source/PaperZ/Props/MovingStaticMeshBaseActor.cpp
@@ -11,7 +11,15 @@ AMovingStaticMeshBaseActor::AMovingStaticMeshBaseActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	SetRootComponent(BaseMesh);
+
+	MovableMesh = CreateDefaultSubobject<UMovableStaticMeshComponent>(TEXT("Movable Mesh"));
+	MovableMesh->SetupAttachment(GetRootComponent());
+
 }
+
+
 
 // Called when the game starts or when spawned
 void AMovingStaticMeshBaseActor::BeginPlay()
@@ -25,5 +33,18 @@ void AMovingStaticMeshBaseActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+FName AMovingStaticMeshBaseActor::GetInteractTag_Implementation()
+{
+	return InteractTag;
+}
+
+void AMovingStaticMeshBaseActor::Interact_Implementation(bool bInteracting)
+{
+	if (!bIsDisabled)
+	{
+		MovableMesh->Move(bInteracting);
+	}
 }
 
--- Source/PaperZ/Props/MovingStaticMeshBaseActor.h
+++ Source/PaperZ/Props/MovingStaticMeshBaseActor.h
@@ -4,16 +4,26 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "PaperZ/Components/MovableStaticMeshComponent.h"
+#include "PaperZ/Interface/InteractableInterface.h"
 #include "MovingStaticMeshBaseActor.generated.h"
 
+
+// This is going to represents our doors, our windows, etc.. The moveable objects itself
+
 UCLASS()
-class PAPERZ_API AMovingStaticMeshBaseActor : public AActor
+class PAPERZ_API AMovingStaticMeshBaseActor : public AActor, public IInteractableInterface
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
 	AMovingStaticMeshBaseActor();
+
+	virtual FName GetInteractTag_Implementation() override;
+	virtual void Interact_Implementation(bool bInteracting) override;
+
+	bool IsDisabled() const { return bIsDisabled; }
 
 protected:
 	// Called when the game starts or when spawned
@@ -22,5 +32,18 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Moving Actor")
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"), Category = "Moving Actor")
+	UMovableStaticMeshComponent* MovableMesh;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Moving")
+	FName InteractTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Moving")
+	bool bIsDisabled;
 
 };
